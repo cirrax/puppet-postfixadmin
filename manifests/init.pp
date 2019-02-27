1a002f9e6501acd2732d3286555ea3baea4e40fc
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2019-02-27 16:00:53 +0100

    fix dependencies to dovecot/postfix class

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,6 +19,12 @@
 #   $aliasdomains
 #     Hash of hases to create postfixadmin::cli::create_aliasdomain
 #     Defaults to {}
+#   $dovecot_classes
+#     Array of classnames that are required for devecot
+#     query installation (defaults to ['dovecot'])
+#   $postfix_classes
+#     Array of classnames that are required for postfix
+#     query installation (defaults to ['postfix'])
 #
 class postfixadmin (
   Boolean $ensure_database = false,
@@ -28,31 +34,41 @@ class postfixadmin (
   Hash $admins          = {},
   Hash $domains         = {},
   Hash $aliasdomains    = {},
+  Array $dovecot_classes = ['dovecot'],
+  Array $postfix_classes = ['postfix'],
 ) inherits postfixadmin::params {
 
+  Class['postfixadmin::install'] -> Class['postfixadmin::config']
+  -> Postfixadmin::Cli::Create_admin <| |>
+  -> Postfixadmin::Cli::Create_domain <| |>
+  -> Postfixadmin::Cli::Create_aliasdomain <| |>
+
+
   if $ensure_database {
+    Class['postfixadmin::install'] -> Class['postfixadmin::db'] 
+    -> Postfixadmin::Cli::Create_admin <| |>
     include ::postfixadmin::db
   }
 
   if $ensure_vhost {
+    Class['postfixadmin::config'] -> Class['postfixadmin::vhost']
     include ::postfixadmin::vhost
   }
 
   if $ensure_postfix_queries {
+    Class[$postfix_classes] -> Class['postfixadmin::queries::postfix']
     include ::postfixadmin::queries::postfix
   }
 
   if $ensure_dovecot_queries {
+    Class[$dovecot_classes] -> Class['postfixadmin::queries::dovecot']
     include ::postfixadmin::queries::dovecot
   }
 
   include ::postfixadmin::install
   include ::postfixadmin::config
 
-  Class['postfixadmin::install'] -> Class['postfixadmin::config']
-
   create_resources('postfixadmin::cli::create_admin', $admins)
   create_resources('postfixadmin::cli::create_domain', $domains)
   create_resources('postfixadmin::cli::create_aliasdomain', $aliasdomains)
-
 }
