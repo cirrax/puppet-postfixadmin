f7afd1b636d05f02e1517eaf875660a45726bfae
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2024-12-25 12:55:04 +0100

    add parameter for mysql db collate/charset

--- manifests/db/mysql.pp
+++ manifests/db/mysql.pp
@@ -3,20 +3,32 @@
 # use postfixadmin::db instead
 #
 # @param dbname
+#   name of the database
 # @param dbuser
+#   username to access the database
 # @param dbpass
+#   password for the database
 # @param host
+#   hostname
+# @param collate
+#   collate for the database
+# @param charset
+#   charset for the database
 #
 class postfixadmin::db::mysql (
-  String $dbname = $postfixadmin::db::dbname,
-  String $dbuser = $postfixadmin::db::dbuser,
-  String $dbpass = $postfixadmin::db::dbpass,
-  String $host   = $postfixadmin::db::host,
+  String              $dbname  = $postfixadmin::db::dbname,
+  String              $dbuser  = $postfixadmin::db::dbuser,
+  String              $dbpass  = $postfixadmin::db::dbpass,
+  String              $host    = $postfixadmin::db::host,
+  Optional[String[1]] $collate = undef,
+  Optional[String[1]] $charset = undef,
 ) inherits postfixadmin::db {
   mysql::db { $dbname :
     user     => $dbuser,
     password => $dbpass,
     host     => $host,
     grant    => ['ALTER', 'CREATE', 'SELECT', 'INSERT', 'UPDATE', 'DELETE'],
+    collate  => $collate,
+    charset  => $charset,
   }
 }
